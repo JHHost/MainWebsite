e28730a1672661e1d5a4b3f77f1e2ad0d77bdf27
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2025-04-15 01:01:06 +1000

    Fixes

--- portfolio/addgames.js
+++ portfolio/addgames.js
@@ -149,11 +149,11 @@ function AddCollapse() {
 
 function SetCollapse() {
 var coll = document.getElementsByClassName("collapsible");
-var i;
 
-for (i = 0; i < coll.length; i++) {
+for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    if (i != 0) {this.classList.toggle("active");}
+    
     var content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
