f3a564887fb269d99fa0d4f42adf1fafe2de3a00
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2025-08-16 23:20:21 +1000

    Multilocation game support

--- portfolio/addgames.js
+++ portfolio/addgames.js
@@ -1,71 +1,98 @@
 var games = {
     0: {
-        "name" : "Guns GirlZ: Operation Gekkou - Retro",
-        "type" : "itch",
-        "link" : "https://hoyostans.itch.io/guns-girlz-operation-gekkou-retro",
+        "name": "Guns GirlZ: Operation Gekkou - Retro",
+        "type": ["itch"],
+        "link": ["https://hoyostans.itch.io/guns-girlz-operation-gekkou-retro"],
         "image": "https://img.itch.zone/aW1nLzIxNjYxOTUwLnBuZw==/315x250%23c/AsGUtZ.png"
     },
     1: {
-        "name" : "Cat Carnival",
-        "type" : "itch",
-        "link" : "https://jayden-holliss.itch.io/cat-carnival",
+        "name": "Cat Carnival",
+        "type": ["itch"],
+        "link": ["https://jayden-holliss.itch.io/cat-carnival"],
         "image": "https://img.itch.zone/aW1nLzE3MzYxNDkzLnBuZw==/original/TVE%2BND.png"
     },
     2: {
-        "name" : "Toy Survival",
-        "type" : "itch",
-        "link" : "https://jayden-holliss.itch.io/toy-survival",
+        "name": "Toy Survival",
+        "type": ["itch"],
+        "link": ["https://jayden-holliss.itch.io/toy-survival"],
         "image": "https://img.itch.zone/aW1nLzE4OTMyMDY4LnBuZw==/315x250%23c/3UJj9Z.png"
     },
     3: {
-        "name" : "Your Screen",
-        "type" : "itch",
-        "link" : "https://jayden-holliss.itch.io/your-screen",
+        "name": "Your Screen",
+        "type": ["itch"],
+        "link": ["https://jayden-holliss.itch.io/your-screen"],
         "image": ""
     },
     4: {
-        "name" : "Bubble Wubble",
-        "type" : "ggj",
-        "link" : "https://globalgamejam.org/games/2025/bubble-wubble-2",
+        "name": "Bubble Wubble",
+        "type": ["ggj"],
+        "link": ["https://globalgamejam.org/games/2025/bubble-wubble-2"],
         "image": ""
     },
     5: {
-        "name" : "Audio Waves",
-        "type" : "itch",
-        "link" : "",
+        "name": "Audio Waves",
+        "type": ["itch"],
+        "link": [""],
         "image": ""
     },
     6: {
-        "name" : "Super Fast Racing",
-        "type" : "itch",
-        "link" : "https://jayden-holliss.itch.io/super-fast-racing",
+        "name": "Super Fast Racing",
+        "type": ["itch"],
+        "link": ["https://jayden-holliss.itch.io/super-fast-racing"],
         "image": "https://img.itch.zone/aW1nLzE5OTU5MTc3LnBuZw==/original/MrHtTV.png"
     },
     7: {
-        "name" : "Endless Jumper",
-        "type" : "google",
-        "link" : "https://play.google.com/store/apps/details?id=com.GoodEnoughToBeTheBest.EndlessJumper",
+        "name": "Endless Jumper",
+        "type": ["google"],
+        "link": ["https://play.google.com/store/apps/details?id=com.GoodEnoughToBeTheBest.EndlessJumper"],
         "image": "https://play-lh.googleusercontent.com/5Y4l6Dj4jI3iTYOPPauvB12LG__GfP4lnTa1nhpzjGQSpT9ajqrZLxhOMUQyVmN39Rc3=w240-h480-rw"
     },
     8: {
-        "name" : "Seeds of Klotho",
-        "type" : "itch",
-        "link" : "https://teamhyposphere.itch.io/seeds-of-klotho",
+        "name": "Seeds of Klotho",
+        "type": ["itch"],
+        "link": ["https://teamhyposphere.itch.io/seeds-of-klotho"],
         "image": "https://img.itch.zone/aW1nLzIxNTc2NTY0LnBuZw==/315x250%23c/Dr4%2BVn.png"
     },
     9: {
-        "name" : "NEEDA History Investigation Series",
-        "type" : "itch",
-        "link" : "https://jayden-holliss.itch.io/needa",
+        "name": "NEEDA History Investigation Series",
+        "type": ["itch"],
+        "link": ["https://jayden-holliss.itch.io/needa"],
         "image": "https://img.itch.zone/aW1nLzIyNzM4ODM5LnBuZw==/347x500/hN93yN.png"
     },
     10: {
-        "name" : "Online Drawing Game",
-        "type" : "itch",
-        "link" : "https://jayden-holliss.itch.io/online-drawing",
+        "name": "Online Drawing Game",
+        "type": ["itch"],
+        "link": ["https://jayden-holliss.itch.io/online-drawing"],
         "image": "https://img.itch.zone/aW1nLzIyNzM4NzU0LnBuZw==/347x500/K9RuRK.png"
     },
 };
+
+
+
+var typeData = {
+    "steam": {
+        gameColour: "#145689",
+        gameType: "Steam"
+    },
+    "google": {
+        gameColour: "#A4C639",
+        gameType: "Google Play Store"
+    },
+    "itch": {
+        gameColour: "#FA5C5C",
+        gameType: "itch.io"
+    },
+    "ggj": {
+        gameColour: "#1FA9E1",
+        gameType: "Global Game Jam"
+    },
+    "default": {
+        gameColour: "#870051",
+        gameType: ""
+
+    }
+}
+
 var typesCompleted = 0;
 var undefinedImage = "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg";
 
@@ -92,51 +119,49 @@ function SetGames(gamesHolder, type) {
         let content = (gamesHolder.innerHTML);
         //console.log(content);
         let gameLink = games[i].link;
-        if (gameLink != "") {
+        if (gameLink != [""]) {
             let gameImage = games[i].image;
             if (gameImage == "") {
                 gameImage = undefinedImage;
-            }
-            let gameColour = '#870051';
-            let gameType = '';
-            switch (games[i].type) {
-                case "steam":
-                    gameColour = "#145689";
-                    gameType = "Steam";
-                    break;
-                case "google":
-                    gameColour = "#A4C639";
-                    gameType = "Google Play Store";
-                    break;
-                case "itch":
-                    gameColour = "#FA5C5C";
-                    gameType = "itch.io";
-                    break;
-                case "ggj":
-                    gameColour = "#1FA9E1";
-                    gameType = "Global Game Jam";
-                    break;
-                default:
-                    gameColour = "#870051";
-                    gameType = '';
-                    break;
 
-            }
-            if (type == 'any' || games[i].type == type) {
-                content += '<div class="gamecontent"> <div class="thumb_wrapper"><h3 style="padding-top:5px;padding-bottom:5px;max-width:200px;font-size:1em;height: 2em;max-height: 2em;min-height: 1em;">'+games[i].name+'</h3><br><a href="'+gameLink+'"><img src="' + gameImage + '" class="thumb" srcset="' + gameImage + ' 2x" style="max-height:200px;min-height:200px;height:200px;"></a><div class="downloadbutton" style="align-items: center;text-align: center;"><a href="' + gameLink + '" class="button" target="_blank" style="background-color:' + gameColour + ';max-width: 180px;text-wrap: auto;">Download on ' + gameType + '</a></div>&lrm;</div><p>&lrm;</div><p>&lrm;'
-            }
+                if (type == 'any' || games[i].type.includes(type)) {
+                    let itemTypes = Object.keys(games[i].type);
+                    let contentTmp = "";
+                    contentTmp += '<div class="gamecontent"> <div class="thumb_wrapper"><h3 style="padding-top:5px;padding-bottom:5px;max-width:200px;font-size:1em;height: 2em;max-height: 2em;min-height: 1em;">' + games[i].name + '</h3><br>';
+                    if (itemTypes.length == 1) {
+                        contentTmp += '<a href="' + gameLink[0] + '"><img src="' + gameImage + '" class="thumb" srcset="' + gameImage + ' 2x" style="max-height:200px;min-height:200px;height:200px;"></a>';
+                    }
+                    else {
+                        contentTmp += '<img src="' + gameImage + '" class="thumb" srcset="' + gameImage + ' 2x" style="max-height:200px;min-height:200px;height:200px;">';
+                    }
+                    contentTmp += '<div style="display: flex; flex-wrap: wrap; justify-content: center;">';
+                    let size = (180 / itemTypes.length);
+                    for (let p = 0; p < itemTypes.length; p++) {
+                        if (type == 'any') {
+                            contentTmp += '<div class="downloadbutton" style="align-items: center;text-align: center;"><a href="' + typeData[gameLink[p]] + '" class="button" target="_blank" style="background-color:' + typeData[gameColour[p]] + ';max-width: ' + size.toString() + 'px;text-wrap: auto;">Download on ' + typeData[gameType[p]] + '</a></div>'
+                        }
+                        else if (type == itemTypes[p]) {
+                            contentTmp += '<div class="downloadbutton" style="align-items: center;text-align: center;"><a href="' + typeData[gameLink[p]] + '" class="button" target="_blank" style="background-color:' + typeData[gameColour[p]] + ';max-width: 180px;text-wrap: auto;">Download on ' + typeData[gameType[p]] + '</a></div>'
+                        }
+
+                    }
+                    contentTmp += '</div>&lrm;</div><p>&lrm;</div><p>&lrm;';
+
+                    content += contentTmp;
+                }
 
 
-            
+
+            }
+            if (content != undefined) {
+                gamesHolder.innerHTML = content;
+            }
+
         }
-        if (content != undefined) {
-            gamesHolder.innerHTML = content;
-        }
-
     }
     typesCompleted++;
-    
-    
+
+
 
 
 
@@ -156,20 +181,20 @@ function AddCollapse() {
 }
 
 function SetCollapse() {
-var coll = document.getElementsByClassName("collapsible");
+    var coll = document.getElementsByClassName("collapsible");
 
-for (let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
-  });
-}
-setTimeout(function(){ coll[0].click(); }, 400);
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+    setTimeout(function () { coll[0].click(); }, 400);
 
 
 }
