6e77b093095c9ef9ba020a3e218bccb0998450af
JaydenHollissSAE <159383304+JaydenHollissSAE@users.noreply.github.com>
2025-12-08 17:52:34 +1100

    Clickable carousel images

---
+++ viewimage.js
@@ -0,0 +1,37 @@
+function ToggleImage(url, opening) {
+    if (opening) {
+        imageView.src = url;
+        imageViewBase.style.visibility = "visible";
+    }
+    else {
+        imageViewBase.style.visibility = "hidden";
+    }
+}
+
+
+let carouselImages = document.getElementsByClassName("carouselItem");
+
+if (carouselImages.length > 0) {
+    fetch("/imageViewBase.html")
+        .then(response => {
+            return response.text()
+        })
+        .then(data => {
+            document.innerHTML = document.innerHTML + data;
+        });
+
+
+    for (let i = 0; i < carouselImages.length; i++) {
+        carouselImages[i].addEventListener("click", function () {
+            ToggleImageOn(carouselImages[i].src, true);
+        });
+    }
+
+    var imageViewBase = document.getElementsByClassName("imageViewBase")[0];
+    var imageView = document.getElementsByClassName("imageView")[0];
+
+    imageViewBase.addEventListener("click", function () {
+        ToggleImageOn(null, false);
+    });
+}
+
