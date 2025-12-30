deb6f1dc1fde3c1e0a50edf0d908d94edb00a549
JKCrafted <78131640+JKCrafted@users.noreply.github.com>
2025-12-30 13:37:22 +1100

    Auto 404 redirects

---
+++ redirects.js
@@ -0,0 +1,24 @@
+var siteRedirects = {
+    "/Games/EndlessJumper/TermsOfService": "/Games/EndlessJumper/PrivacyPolicy/",
+    "/EndlessJumper/TermsOfService": "/Games/EndlessJumper/PrivacyPolicy/",
+    "/EndlessJumper/PrivacyPolicy": "/Games/EndlessJumper/PrivacyPolicy/",
+    "/Games/CatCarnival/Credits": "/Games/CatCarnival"
+}
+
+
+
+var siteRedirectsKeys = Object.keys(siteRedirects);
+
+var currentUrl = window.location.href.remove("https://jaydenholliss.com.au");
+
+if (siteRedirectsKeys.contains(currentUrl)) {
+    window.location.replace(siteRedirects[currentUrl]);
+}
+
+else if (currentUrl[currentUrl.length-1] == "/") {
+    currentUrl = currentUrl.substring(0, currentUrl.length-1)
+    if (siteRedirectsKeys.contains(currentUrl)) {
+        window.location.replace("https://jaydenholliss.com.au"+siteRedirects[currentUrl]);
+    }
+}
+
